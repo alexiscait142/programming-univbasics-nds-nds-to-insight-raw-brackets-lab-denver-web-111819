bb1d0504c9c8cc2073708a96757a566a92b7dc29
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-26 02:56:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,10 +22,10 @@ def directors_totals(nds)
   director_index = 0 
   while director_index < nds.length do
     name_index = nds[director_index][:name]
-    hash[name_index] = nil 
+    hash[name_index] = 0
     movie_index = 0
     while movie_index < nds[director_index][:movies].length do
-      hash[name_index] = nds[director_index][:movies][movie_index][:worldwide_gross]
+      hash[name_index] += nds[director_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
     end
     director_index += 1
